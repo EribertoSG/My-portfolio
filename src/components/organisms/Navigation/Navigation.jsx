446401a02d2d68996eac6eb8586c1994de048519
Eriberto-SG <eribertosg.dev@gmail.com>
2023-06-20 14:54:54 -0600

    Se personalizo el scroll, ademas se le agrego personalizo la estructura del idioma y se le agrego el icono de chevron down.

--- src/components/organisms/Navigation/Navigation.jsx
+++ src/components/organisms/Navigation/Navigation.jsx
@@ -14,7 +14,10 @@ const Navigation = () => {
                         <Link className={styles.item} href="/projects">Projects</Link>
                         <Link className={styles.item} href="/about">About me</Link>
                         <Link className={styles.item} href="/contact">Contact</Link>
-                        <Link className={styles.item} href="/idioma">Es v</Link>
+                        <div className={styles.item}>
+                            <span>EN</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.chevronDown} width="24" height="24" viewBox="0 0 24 24"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z" /></svg>
+                        </div>
                     </nav>
 
                     <div className={styles.cv}>
