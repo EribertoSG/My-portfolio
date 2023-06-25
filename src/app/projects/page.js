3aa2403c1ee8e15f3470d0f0e2f2fb6c2e7d00de
Eriberto-SG <eribertosg.dev@gmail.com>
2023-06-24 22:16:03 -0600

    Se agrego algunas restriciones al card para que solo muestre 4 lineas en desktop y 3 en movil en parrafos y para el titulo solo 2 lineas.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,8 +6,8 @@ export default function page() {
         {
             id: 1,
             src: '/projects/unsplash.png',
-            title: 'Aqui va el titulo',
-            description: 'La description'
+            title: 'Aqui va el titulo y si es demasiado largo se tiene que ocultar el resto del titulo y todo lo demas ',
+            description: 'La description de este proyecto es demasiado larga, asi que se necesita que se restriga a 4 lineas. Al final ponerle un especie de ellipsis para que se vea chingona. Es necesario que se vea bien en todo los tamaños.    '
         },
         {
             id: 1,
