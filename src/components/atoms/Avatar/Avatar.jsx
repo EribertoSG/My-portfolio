c915f9bd80dbafdcd885e144eb1f108e761d824e
ESG <eribertosg.dev@gmail.com>
2023-09-21 23:00:23 -0600

    Se agrego el efecto blur al cargarse la img

--- src/components/atoms/Avatar/Avatar.jsx
+++ src/components/atoms/Avatar/Avatar.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from '@/components/atoms/Avatar/avatar.module.css'
 import Image from 'next/image'
 
 const Avatar = ({ src, width, height, alt }) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Función para manejar la carga completa de la imagen
+    const handleImageLoad = () => {
+        setIsLoading(true);
+    };
+
     return (
         <div className={styles.avatar}>
-            <span className={styles.imageContainer}>
-                <Image src={src} width={width} height={height} alt={alt} />
+            <span className={`${styles.imageContainer} ${styles.loadingImage} ${isLoading ? styles.blur : ''}`}>
+                <Image src={src} width={width} height={height} alt={alt} onLoadingComplete={handleImageLoad} />
             </span>
         </div>
     )
--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '@/components/molecules/Card/card.module.css'
+'use client'
+// Importa useState
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/components/molecules/Card/card.module.css';
 
 const Card = ({ src, title, description, href }) => {
+    // Agrega un estado para controlar si la imagen se ha cargado
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Función para manejar la carga completa de la imagen
+    const handleImageLoad = () => {
+        setIsLoading(true);
+    };
+
     return (
         <article className={styles.card}>
             <div className={styles.cardHeader}>
                 <Link href={href}>
-                    <Image src={src} width={450} height={250} alt='my-portfolio' />
+                    {/* Aplica un efecto de desenfoque si isLoading es true */}
+                    <div className={`${styles.loadingImage} ${isLoading ? styles.blur : ''}`}>
+                        <Image
+                            src={src}
+                            width={450}
+                            height={250}
+                            alt='my-portfolio'
+                            onLoadingComplete={handleImageLoad}
+                        />
+                    </div>
                 </Link>
             </div>
             <div className={styles.cardBody}>
-                <Link href={href}><h3>{title}</h3></Link>
+                <Link href={href}>
+                    <h3>{title}</h3>
+                </Link>
                 <p>{description}</p>
             </div>
         </article>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
